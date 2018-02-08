695ef70eddadaef8157a6c23fc4ef3e0f07bb786
Emilio <devemilio@hotmail.com>
2018-02-07 23:31:47 -0600

    initial release

---
+++ src/main/java/us/zonix/hcfactions/misc/commands/StackCommand.java
@@ -0,0 +1,20 @@
+package us.zonix.hcfactions.misc.commands;
+
+import org.bukkit.entity.Player;
+import us.zonix.hcfactions.util.PluginCommand;
+import us.zonix.hcfactions.util.command.Command;
+import us.zonix.hcfactions.util.command.CommandArgs;
+import us.zonix.hcfactions.util.PluginCommand;
+import us.zonix.hcfactions.util.command.Command;
+import us.zonix.hcfactions.util.command.CommandArgs;
+
+public class StackCommand extends PluginCommand {
+
+    @Command(name = "stack", aliases = {"more"}, inGameOnly = true, permission = "command.stack")
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        player.getInventory().getItemInHand().setAmount(64);
+    }
+
+
+}
---
+++ src/main/java/us/zonix/hcfactions/profile/options/item/ProfileOptionsItemState.java
@@ -0,0 +1,9 @@
+package us.zonix.hcfactions.profile.options.item;
+
+public enum ProfileOptionsItemState {
+    ENABLED,
+    DISABLED,
+    TAB_DETAILED,
+    TAB_DETAILED_VANILLA,
+    TAB_VANILLA
+}
